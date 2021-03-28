afbfb28fdb3230dc33f8526af57a5f00b27e4fd4
Brian Ketelsen <bketelsen@gmail.com>
2021-03-28 16:16:14 -0400

    good luck, charlie brown

--- api/data.js
+++ api/data.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const outputroot = '../output/profiles/';
+const outputroot = path.join(__dirname, "output");
 
 
 const files = fs.readdirSync(path.join(outputroot, "profiles"));
 const acc = [];
 files.forEach((curr) => {
     if (curr.includes('.json')) {
-        const file = JSON.parse(fs.readFileSync(path.join(outputroot, curr), 'utf8'));
+        const file = JSON.parse(fs.readFileSync(path.join(outputroot, "profiles", curr), 'utf8'));
         const extension = path.extname(curr);
         const slug = path.basename(curr, extension);
         // remove wrapping key
