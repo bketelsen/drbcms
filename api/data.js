722ea72d7943f0896ca1b0f3f2570dea36911175
Brian Ketelsen <bketelsen@gmail.com>
2021-03-29 05:31:26 -0400

    added several content types

--- api/data.js
+++ api/data.js
@@ -3,23 +3,29 @@ const path = require('path');
 
 const outputroot = path.join(__dirname, "../.", "output");
 
+const content_types = ["articles", "categories", "pages", "profiles", "sites"]
 
-const files = fs.readdirSync(path.join(outputroot, "profiles"));
-const acc = [];
-files.forEach((curr) => {
-    if (curr.includes('.json')) {
-        const file = JSON.parse(fs.readFileSync(path.join(outputroot, "profiles", curr), 'utf8'));
-        const extension = path.extname(curr);
-        const slug = path.basename(curr, extension);
-        // remove wrapping key
-        var stripped = file[slug];
-        // add id property
-        stripped.id = stripped.basename;
-        acc.push(stripped);
-    }
+var data = {}
 
-}, {});
+content_types.forEach((ct) => {
 
-module.exports = {
-    profiles: acc
-}
+    const files = fs.readdirSync(path.join(outputroot, ct));
+    const acc = [];
+    files.forEach((curr) => {
+        if (curr.includes('.json')) {
+            const file = JSON.parse(fs.readFileSync(path.join(outputroot, ct, curr), 'utf8'));
+            const extension = path.extname(curr);
+            const slug = path.basename(curr, extension);
+            // remove wrapping key
+            var stripped = file[slug];
+            // add id property
+            stripped.id = stripped.basename;
+            acc.push(stripped);
+        }
+
+    }, {});
+    data[ct] = acc;
+
+
+}, {})
+module.exports = data;
